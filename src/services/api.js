02b690e9e44d8fa5dc5d6f9f8a293d228689f9f3
athibau2 <a.thibs98@gmail.com>
2022-03-02 11:15:09 -0700

    create org

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,29 @@ import { authHeader, getJwtToken, getUserIdFromToken } from "./auth";
 const API_URL = "http://localhost:8000";
 
 class Api {
+
+  async createOrganization(orgname) {
+    await axios.post(
+      API_URL + "/organization",
+      {
+        orgname: orgname
+      },
+      {
+        headers: authHeader(),
+      }
+    )
+    return await axios.post(
+      API_URL + "/part_of",
+      {
+        userid: getUserIdFromToken(getJwtToken()),
+        orgid: orgid // get orgId somehow
+      },
+      {
+        headers: authHeader()
+      }
+    )
+  }
+
   getArticles() {
     return axios.get(API_URL + "/articles", {});
   }
@@ -56,8 +79,8 @@ class Api {
     return axios.post(API_URL + "/rpc/login", { email, password });
   }
 
-  signup(email, password, name) {
-    return axios.post(API_URL + "/rpc/signup", { email, password, name });
+  signup(firstname, lastname, email, password) {
+    return axios.post(API_URL + "/rpc/signup", { firstname, lastname, email, password });
   }
 }
 
