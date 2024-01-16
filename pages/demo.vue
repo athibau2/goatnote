c228601ccc81c06c123cdb5c38d329235726684f
athibau2 <a.thibs98@gmail.com>
2024-01-16 15:58:46 -0700

    small edit to quiz logic

--- pages/demo.vue
+++ pages/demo.vue
@@ -61,7 +61,7 @@
         </div>
 
         <!-- Quiz form -->
-        <div v-if="tryQuiz"
+        <div v-if="tryQuiz && quizQuestions.length == 0"
             style="display: flex; flex-direction: column;
             justify-content: center; align-items: center; margin-top: 20px;"
             data-aos="fade-down"
